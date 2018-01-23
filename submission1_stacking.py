cfe582a8b78f21a558e94e908e0be660f2541e85
Yingjie Yu <eileenlaoyu@163.com>
2018-01-23 15:37:15 -0800

    Add files via upload

--- submission1_stacking.py
+++ submission1_stacking.py
@@ -19,6 +19,7 @@ from sklearn.linear_model import Ridge,SGDRegressor
 from sklearn.svm import LinearSVR
 from sklearn.tree import DecisionTreeRegressor 
 
+import lightgbm as lgb
 
 from scipy.sparse import coo_matrix, hstack,csr_matrix
 import gc
@@ -47,13 +48,18 @@ gc.collect()
 #%% 
 # dealing with brand_name
 df['brand_name'].fillna(value='unknown', inplace=True)
-x_brand = df['brand_name']
-lb = LabelBinarizer(sparse_output=True)
-x_brand_lb = lb.fit_transform(x_brand)
+# cut brand_name
+POP_BRAND = df['brand_name'].value_counts().loc[lambda x: x.index != 'unknown'].index[: 4000]
+df.loc[~df['brand_name'].isin(POP_BRAND),'brand_name'] = 'unknown'
 
-x_brand = x_brand.astype('str')
-fh = FeatureHasher(n_features=3000,input_type="string")
-x_brand_hash = fh.fit_transform(x_brand)
+lb = LabelBinarizer(sparse_output=True)
+X_brand = lb.fit_transform(df['brand_name'])
+print('Binarize brand_name complete')
+
+#X_brand = X_brand.astype('str')
+#fh = FeatureHasher(n_features=3000,input_type="string")
+#X_brand_hash = fh.fit_transform(X_brand)
+
 
 #%%
 # dealing with catgorical_name
@@ -64,43 +70,48 @@ def split_text(text):
     except: return ('No Label','No Label','No Label')
 df['general_cat'],df['subcat_1'],df['subcat_2'] = zip(*df['category_name'].apply(lambda x: split_text(x)))
 
-x_cat1 = df['general_cat']
-x_cat2 = df['subcat_1']
-x_cat3 = df['subcat_2']
+# cutting
+# the number of categories are 11/114/883
+# so I decided to cut only subcat_2
+POP_CAT2 = df['subcat_2'].value_counts().loc[lambda x: x.index!='unknown'].index[:500]
+df.loc[~df['subcat_2'].isin(POP_CAT2),'subcat_2'] = 'unknown'
 
-x_cat1 = x_cat1.astype('category')
-x_cat2 = x_cat2.astype('category')
-x_cat3 = x_cat3.astype('category')
 
-lb_cat = LabelBinarizer()
-x_cat1 = lb_cat.fit_transform(x_cat1)
-x_cat2 = lb_cat.fit_transform(x_cat2)
-x_cat3 = lb_cat.fit_transform(x_cat3)
+df['general_cat'] = df['general_cat'].astype('category')
+df['subcat_1'] = df['subcat_1'].astype('category')
+df['subcat_2'] = df['subcat_2'].astype('category')
 
-x_cat_all = np.hstack((x_cat1,x_cat2, x_cat3))
+cv_cat = CountVectorizer()
+X_cat1 = cv_cat.fit_transform(df['general_cat'])
+X_cat2 = cv_cat.fit_transform(df['subcat_1'])
+X_cat3 = cv_cat.fit_transform(df['subcat_2'])
 
-x_cat_all = scipy.sparse.csr_matrix(x_cat_all)
+X_cat_all = scipy.sparse.hstack((X_cat1,X_cat2, X_cat3))
+
 
 svd = TruncatedSVD(n_components=100,n_iter= 3,algorithm='randomized')
-x_cat_all= svd.fit_transform(x_cat_all)
-#sorted(svd.explained_variance_ratio_)
-#sum(svd.explained_variance_ratio_) # 87%
+X_cat_all= svd.fit_transform(X_cat_all)
+X_cat_all = scipy.sparse.csr_matrix(X_cat_all)
 
+#sorted(svd.explained_variance_ratio_)
+#sum(svd.explained_variance_ratio_) # 88%
+print('catogorical_name completed')
 #%%
 # dealing with item_condition_id
-x_ici = pd.get_dummies(df['item_condition_id'],sparse = True)
-x_ici = scipy.sparse.csr_matrix(x_ici.values)
+X_ici = pd.get_dummies(df['item_condition_id'],sparse = True)
+X_ici = scipy.sparse.csr_matrix(X_ici.values)
 
 #%%
 #dealing with shipping
-x_shipping = pd.get_dummies(df['shipping'],sparse = True)
-x_shipping = scipy.sparse.csr_matrix(x_shipping.values)
+X_shipping = pd.get_dummies(df['shipping'],sparse = True)
+X_shipping = scipy.sparse.csr_matrix(X_shipping.values)
 
+print('get dummy of item_condition_id and shipping completed')
 #%%
 #dealing with name
-count = CountVectorizer(min_df=12)
-x_name_count = count.fit_transform(df["name"])
-
+name_cv = CountVectorizer(min_df=10)
+X_name_count = name_cv.fit_transform(df["name"])
+print('count vectorize name done')
 #%%
 # dealing with item_description
 stop = set(stopwords.words('english'))
@@ -126,27 +137,52 @@ def tokenize(text):
     except TypeError as e: print(text,e)
     
 df['item_description'].fillna(value='unknown', inplace=True)
-desc_tfidf = TfidfVectorizer(min_df = 10, max_features= 100000,tokenizer=tokenize,ngram_range = (1,3))
-x_desc = desc_tfidf.fit_transform(df['item_description']) 
-
+desc_tfidf = TfidfVectorizer(min_df = 10, max_features= 50000,tokenizer=tokenize,ngram_range = (1,3))
+X_desc = desc_tfidf.fit_transform(df['item_description']) 
+print('tf-idf item_description done')
 #%%
 # putting all together
-X = scipy.sparse.hstack((x_ici,x_shipping, x_cat_all, x_desc,x_brand_hash,x_name_count)).tocsr()
-#save_sparse_csr('X_test',X_test)
+X = hstack((X_ici,X_shipping, X_cat_all, X_desc,X_brand.astype(int),X_name_count)).tocsr()
+
 X_train = X[:nrow_train]
 X_test = X[nrow_train:]
 
-#%% 
-# Building the first Ridge Model
-rg1 = Ridge(solver='sag', alpha = 3, fit_intercept=True)
-rg1.fit(X_train, log_y)
-pred_rg1 = rg1.preidict(X_test)
+del X
+del df
+gc.collect()
+#%% lgb model
+"""for best fit of lightGBM
+num_leaves : This parameter is used to set the number of leaves to be formed in a tree. Theoretically relation between num_leaves and max_depth is num_leaves= 2^(max_depth). However, this is not a good estimate in case of Light GBM since splitting takes place leaf wise rather than depth wise. Hence num_leaves set must be smaller than 2^(max_depth) otherwise it may lead to overfitting. Light GBM does not have a direct relation between num_leaves and max_depth and hence the two must not be linked with each other.
+min_data_in_leaf : It is also one of the important parameters in dealing with overfitting. Setting its value smaller may cause overfitting and hence must be set accordingly. Its value should be hundreds to thousands of large datasets.
+max_depth: It specifies the maximum depth or level up to which tree can grow.
+"""
+
+lightgbm_params = {
+        'learning_rate': 0.624,
+        'application': 'regression',
+        'max_depth': 3,
+        'num_leaves': 99,
+        'verbosity': 10,
+        'metric': 'RMSE',
+        'nthread': 4
+    }
+
+train_X, valid_X, train_y, valid_y = train_test_split(X_train, log_y, test_size = 0.15, random_state = 44) 
+d_train = lgb.Dataset(train_X, label=train_y, max_bin=8192)
+d_valid = lgb.Dataset(valid_X, label=valid_y, max_bin=8192)
+watchlist = [d_train, d_valid]
+
+
+lgb_model = lgb.train(lightgbm_params, train_set=d_train, num_boost_round=7500, valid_sets=watchlist, early_stopping_rounds=50, verbose_eval=500) 
+lightgbm_train = lgb_model.predict(X_train)
+lightgbm_test = lgb_model.predict(X_test)
+
+print('lgb done')
 
 #%% Stacking by hand
-from sklearn.ensemble import (RandomForestClassifier, AdaBoostClassifier, 
-                              GradientBoostingClassifier, ExtraTreesClassifier)
-from sklearn.svm import SVC
-from sklearn.cross_validation import KFold
+#from sklearn.cross_validation import KFold
+from sklearn.model_selection import KFold
+
 
 ntrain = X_train.shape[0]
 ntest = X_test.shape[0]
@@ -203,41 +239,23 @@ def get_oof(reg, X_train, log_y, X_test, lightgbm = False):
 sgd_params = {'penalty':'l2'}
 lsvr_params = {'epsilon':1.5}
 dt_params = {'max_depth':2,'min_samples_leaf':10}
-"""for best fit of lightGBM
-num_leaves : This parameter is used to set the number of leaves to be formed in a tree. Theoretically relation between num_leaves and max_depth is num_leaves= 2^(max_depth). However, this is not a good estimate in case of Light GBM since splitting takes place leaf wise rather than depth wise. Hence num_leaves set must be smaller than 2^(max_depth) otherwise it may lead to overfitting. Light GBM does not have a direct relation between num_leaves and max_depth and hence the two must not be linked with each other.
-min_data_in_leaf : It is also one of the important parameters in dealing with overfitting. Setting its value smaller may cause overfitting and hence must be set accordingly. Its value should be hundreds to thousands of large datasets.
-max_depth: It specifies the maximum depth or level up to which tree can grow.
-"""
 
-lightgbm_params = {
-        'learning_rate': 0.624,
-        'application': 'regression',
-        'max_depth': 3,
-        'num_leaves': 99,
-        'verbosity': 10,
-        'metric': 'RMSE',
-        'nthread': 4
-    }
 
 
 
 #ridge = SklearnHelper(reg = Ridge, seed = SEED, params=ridge_params)
 sgd = SklearnHelper(reg = SGDRegressor, seed = SEED, params=sgd_params)
+
 lsvr = SklearnHelper(reg = LinearSVR, seed = SEED, params = lsvr_params)
 dt = SklearnHelper(reg = DecisionTreeRegressor, seed = SEED, params = dt_params)
 
-
-train_X, valid_X, train_y, valid_y = train_test_split(X_train, log_y, test_size = 0.16, random_state = 44) 
-d_train = lgb.Dataset(train_X, label=train_y, max_bin=8192)
-d_valid = lgb.Dataset(valid_X, label=valid_y, max_bin=8192)
-watchlist = [d_train, d_valid]
  
 
 #ridge_oof_train, ridge_oof_test = get_oof(ridge, X_train, y_train, X_test)
 sgd_oof_train, sgd_oof_test = get_oof(sgd, X_train, log_y, X_test)
 lsvr_oof_train, lsvr_oof_test = get_oof(lsvr, X_train, log_y, X_test)
 dt_oof_train, dt_oof_test = get_oof(dt, X_train, log_y, X_test)
-lightgbm_oof_train, lightgbm_oof_test = get_oof(lgb, X_train, log_y, X_test,lightgbm = True)
+#lightgbm_oof_train, lightgbm_oof_test = get_oof(lgb, X_train, log_y, X_test,lightgbm = True)
 
 #np.savetxt('lightgbm_oof_test.txt',lightgbm_oof_test,delimiter=',')
 
@@ -247,8 +265,11 @@ lightgbm_oof_train, lightgbm_oof_test = get_oof(lgb, X_train, log_y, X_test,ligh
 #                                      'linearSVR': lsvr_oof_train.ravel(),
 #                                      'DecitionTree': dt_oof_train.ravel()})
 #base_prediction_train.head()
-x_train_2 = np.concatenate((sgd_oof_train,lsvr_oof_train,dt_oof_train,lightgbm_oof_train), axis = 1)
-x_test_2 = np.concatenate((sgd_oof_test, lsvr_oof_test, dt_oof_test,lightgbm_oof_test), axis = 1)
+lightgbm_train_reshape = lightgbm_train.reshape(1482535,1)
+lightgbm_test_reshape = lightgbm_test.reshape(693359,1)
+
+x_train_2 = np.concatenate((sgd_oof_train,lsvr_oof_train,dt_oof_train,lightgbm_train_reshape), axis = 1)
+x_test_2 = np.concatenate((sgd_oof_test, lsvr_oof_test, dt_oof_test,lightgbm_test_reshape), axis = 1)
 
 # make predictionn using randomforestregressor
 #regr2 = RandomForestRegressor(n_jobs=-1,n_estimators=100,max_depth=2,min_samples_leaf=10,verbose=1)
@@ -262,3 +283,4 @@ ridge.fit(x_train_2, log_y)
 pred = ridge.predict(x_test_2)
 submission['price'] = np.expm1(pred)
 submission.to_csv("submission_1.csv", index=False)
+
